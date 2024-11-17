4c5905507df899f99fa0e9d076ff992e2ced625a
Pradyuman <pradyumaann@gmail.com>
2024-11-17 04:57:30 +0000

    run file implementation

---
+++ run_file.py
@@ -0,0 +1,19 @@
+from main import CreditRiskModel
+
+# Initialize the model
+model = CreditRiskModel()
+
+# Example data (you would replace this with real client data)
+financial_data = {
+    'current_assets': 1000000,
+    'current_liabilities': 400000,
+    'inventory': 300000,
+    'total_debt': 800000,
+    'total_assets': 2000000,
+    'ebitda': 500000,
+    'debt_service': 200000,
+    'net_income': 300000,
+    'ebit': 400000,
+    'interest_expense': 100000
+}
+
